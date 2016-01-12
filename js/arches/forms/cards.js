b10a594c9c07200485b22d67c61fcd2b82cac51a
Dennis Wuthrich <dwuthrich@fargeo.com>
2016-01-12 09:23:55 -0500

    Updated Card Examples

---
+++ js/arches/forms/cards.js
@@ -0,0 +1,103 @@
+// Summernote Editor
+	$('#condition-summernote').summernote({
+		height: 150,
+		placeholder: 'placeholder text'
+	});
+
+
+// Date Picker
+	$('#condition-dp input').datepicker();
+
+
+// Disturbance Multiselect
+	$('#disturbance-multiselect').chosen({width:'100%'});
+	$('#recomend-multiselect').chosen({width:'100%'});
+	$('#threat-multiselect').chosen({width:'100%'});
+
+
+// Help Panel
+	$('#address-help').on('click', function (ev) {
+		ev.preventDefault();
+
+		// Show help/thesaurus panel
+		$('.arches-help-container').css('left', '80%');
+		$('.arches-form-container').css('right', '19%');
+
+		
+	});
+
+
+//	Addresses Card
+	$('#address-add').on('click', function (ev) {
+		ev.preventDefault();
+
+		// Card Alert (Uncomment to show alert panel and notification text)
+		// $.niftyNoty({
+		//     type: 'danger',
+		//     container : '#address-alert',
+		//     html : '<h4 class="alert-title">Oh snap! You got an error!</h4><p class="alert-message">Change this and that and try again. Duis mollis, est non commodo luctus.</p><div class="mar-top"><button type="button" class="btn btn-danger" data-dismiss="noty">Close this notification</button></div>',
+		//     closeBtn : false
+		// });
+
+		// Show results grid
+		//$('#address-results').addClass('in');
+		$('#address-results').fadeIn(2000);
+		//$('#address-results').removeClass('hidden').delay(1000);
+		
+	});
+
+
+//	Condition Wizard
+	$('#condition-start').on('click', function (ev) {
+		ev.preventDefault();
+
+		// Show wizard forms
+		$('#condition-forms').addClass('in');
+		$('#condition-forms').removeClass('hidden').delay(1000);
+		
+	});
+
+	$('#condition-add').on('click', function (ev) {
+		ev.preventDefault();
+
+		// Show results tiles
+		$('#condition-results').addClass('in');
+		$('#condition-results').removeClass('hidden');
+
+	});
+
+
+// Map Form
+	$('#geometry-creation').on('change', function(){
+		
+		$('#geometry-related-nodes').addClass('in');
+
+
+	});
+
+	$('#close-form').on('click', function(){
+
+		$('#geometry-related-nodes').removeClass('in');
+		$('#map-results').addClass('in');
+		$('#map-results').removeClass('hidden');
+
+
+	});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
--- js/arches/forms/wizards.js
+++ js/arches/forms/wizards.js
@@ -1,64 +1,121 @@
 
-// Arches JS to manage hidden panels for Geometry and File input forms
-	$('#geometry-creation').on('change', function(){
-		
-		$('#geometry-related-nodes').addClass('in');
-
-	});
-
-	$('#close-form').on('click', function(){
-
-		$('#geometry-related-nodes').removeClass('in');
-	});
-
-
-// DROPZONE.JS
-	// =================================================================
-	// Require Dropzone
-	// http://www.dropzonejs.com/
-	// =================================================================
-	Dropzone.options.demoDropzone = { // The camelized version of the ID of the form element
-		// The configuration we've talked about above
-		autoProcessQueue: false,
-		//uploadMultiple: true,
-		//parallelUploads: 25,
-		//maxFiles: 25,
-
-		// The setting up of the dropzone
-		init: function() {
-		var myDropzone = this;
-		//  Here's the change from enyo's tutorial...
-		//  $("#submit-all").click(function (e) {
-		//  e.preventDefault();
-		//  e.stopPropagation();
-		//  myDropzone.processQueue();
-			//
-		//}
-		//    );
-
-
-			this.on('addedfile', function() {
-
-				$('#file-related-nodes').addClass('in').delay(1500);
-
-			});
-
-			$('#close-file-form').on('click', function(){
-				$('#file-related-nodes').removeClass('in');
-				$('.dz-preview').remove();
-				$('#demo-dropzone').removeClass('dz-started');
-
-			});
 
 
 
+	// DROPZONE.JS
+		// =================================================================
+		// Require Dropzone
+		// http://www.dropzonejs.com/
+		// =================================================================
+		Dropzone.options.demoDropzone = { // The camelized version of the ID of the form element
+			// The configuration we've talked about above
+			autoProcessQueue: false,
+			//uploadMultiple: true,
+			//parallelUploads: 25,
+			//maxFiles: 25,
+
+			// The setting up of the dropzone
+			init: function() {
+			var myDropzone = this;
+			//  Here's the change from enyo's tutorial...
+			//  $("#submit-all").click(function (e) {
+			//  e.preventDefault();
+			//  e.stopPropagation();
+			//  myDropzone.processQueue();
+				//
+			//}
+			//    );
+
+				this.on('addedfile', function() {
+
+					$('#file-related-nodes').addClass('in').delay(1500);
+
+				});
+
+				$('#close-file-form').on('click', function(){
+					$('#file-related-nodes').removeClass('in');
+					$('.dz-preview').remove();
+					$('#demo-dropzone').removeClass('dz-started');
+
+				});
+
+			}	
 		}
 
 		
-	}
+	// Wizard
+		// =================================================================
+		// Require Bootstrap Wizard
+		// http://vadimg.com/twitter-bootstrap-wizard-example/
+		// =================================================================	
+		$('#main-wz').bootstrapWizard({
+			tabClass		: 'wz-steps',
+			nextSelector	: '.next',
+			previousSelector	: '.previous',
+			onTabClick: function(tab, navigation, index) {
+				return false;
+			},
+			onInit : function(){
+				$('#main-wz').find('.finish').hide().prop('disabled', true);
+			},
+			onTabShow: function(tab, navigation, index) {
+				var $total = navigation.find('li').length;
+				var $current = index+1;
+				var $percent = ($current/$total) * 100;
+				var wdt = 100/$total;
+				var lft = wdt*index;
 
-	
-  	
+
+
+				$('#main-wz').find('.progress-bar').css({width:wdt+'%',left:lft+"%", 'position':'relative', 'transition':'all .5s'});
+
+
+				// If it's the last tab then hide the last button and show the finish instead
+				if($current >= $total) {
+					$('#main-wz').find('.next').hide();
+					$('#main-wz').find('.finish').show();
+					$('#main-wz').find('.finish').prop('disabled', false);
+				} else {
+					$('#main-wz').find('.next').show();
+					$('#main-wz').find('.finish').hide().prop('disabled', true);
+				}
+			}
+		});
+
+
+		$('#address').bootstrapWizard({
+			tabClass		: 'wz-steps',
+			nextSelector	: '.next',
+			previousSelector	: '.previous',
+			onTabClick: function(tab, navigation, index) {
+				return false;
+			},
+			onInit : function(){
+				$('#address').find('.finish').hide().prop('disabled', true);
+			},
+			onTabShow: function(tab, navigation, index) {
+				var $total = navigation.find('li').length;
+				var $current = index+1;
+				var $percent = ($current/$total) * 100;
+				var wdt = 100/$total;
+				var lft = wdt*index;
+
+
+
+				$('#address').find('.progress-bar').css({width:wdt+'%',left:lft+"%", 'position':'relative', 'transition':'all .5s'});
+
+
+				// If it's the last tab then hide the last button and show the finish instead
+				if($current >= $total) {
+					$('#address').find('.next').hide();
+					$('#address').find('.finish').show();
+					$('#address').find('.finish').prop('disabled', false);
+				} else {
+					$('#address').find('.next').show();
+					$('#address').find('.finish').hide().prop('disabled', true);
+				}
+			}
+		});
   	
 
 

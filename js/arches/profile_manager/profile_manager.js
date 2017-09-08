c9d6920b25adfb4a65107230b65f083bbecb39eb
Dennis Wuthrich <dwuthrich@fargeo.com>
2017-09-07 17:54:01 -0700

    Cleaned up template for profile manager

---
+++ js/arches/profile_manager/profile_manager.js
@@ -0,0 +1,67 @@
+//	JavaScript to Support Arches Graph/Resource Configuration
+//	Version: 0.1
+//
+
+
+//	Widgets	
+
+
+
+// Toggle Resource/Records
+	$('#related-resources-open-btn').on('click', function (ev) {
+		ev.preventDefault();
+		
+		//toggle panels
+		$("#current-resource-panel").addClass('hidden');	
+		$("#related-resources-panel").removeClass('hidden');	
+
+		//force map to show after div is no longer hidden
+		$("#search-map-test").css("position", "inherit");
+		window.dispatchEvent(new Event('resize'));
+	
+
+	});
+
+
+	$('#related-resources-close-btn').on('click', function (ev) {
+		ev.preventDefault();
+		
+		//toggle panels
+		$("#current-resource-panel").removeClass('hidden');	
+		$("#related-resources-panel").addClass('hidden');	
+	
+
+	});
+
+
+
+// Manage panels
+	$('#lnk-resource-records').on('click', function (ev) {
+		ev.preventDefault();
+		
+		//toggle panels
+		$("#resource-records").removeClass('hidden');
+		$("#resource-list").addClass('hidden');	
+	
+		//toggle link display
+		$("#lnk-resource-records").addClass('active');
+		$("#lnk-resource-list").removeClass('active');
+
+	});
+
+
+
+
+
+
+
+
+//	Edit History
+	$('#history-manager').on('click', function (ev) {
+		ev.preventDefault();
+		
+		$("#current-resource-panel").addClass('hidden');
+		$("#edit-history-panel").removeClass('hidden');
+		$("#menu-panel").css("display", "none");
+
+	});

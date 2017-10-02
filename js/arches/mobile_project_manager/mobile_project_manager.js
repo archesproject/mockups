c7820e9063cf5fbae2fcd90dcac9d9c79a114388
Dennis Wuthrich <dwuthrich@fargeo.com>
2017-10-02 14:19:14 -0700

    mobile project manager (mpm) UI

--- js/arches/mobile_project_manager/mobile_project_manager.js
+++ js/arches/mobile_project_manager/mobile_project_manager.js
@@ -13,41 +13,8 @@
 
 	});	
 
-
-
-//	Add Function
-	$('.add-function-button').on('click', function (ev) {
-
-		//get id of user selected function
-		// var functionID = ev.target.id;
-		// var functionNameID = functionID + "-name";
-		// var parent = "'#" + functionNameID + "'";
-
-		//Hide default message in Functions list, add entry
-		$(".left-column-message").addClass("hidden");
-		$(".library-card").removeClass("hidden");
-
-	});
-
-
-
-//	Clicking on a function in the function list should act like clicking the show function library button
-	$('.library-card').on('click', function (ev) {
-
-		//if function library is open, close it and update show library button
-		if ($('#show-functions').text() == 'Close Function Library') {
-			
-			//User Wants to close library panel
-			$("#function-library").addClass("hidden");
-			$("#function-configuration").removeClass("hidden");
-
-			//Update button text
-			$('#show-functions').text('Show Function Library');
-
-		}
-
-	});	
-
+// http://abpetkov.github.io/switchery/
+	new Switchery(document.getElementById('demo-sw-unchecked'), { size: 'small' });
 
 
 

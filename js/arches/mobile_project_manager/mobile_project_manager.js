221f2f8a6b3d38688567aaa4af0664eca5dc18bf
Dennis Wuthrich <dwuthrich@fargeo.com>
2017-10-04 20:46:44 -0700

    More work on resource and people tab

--- js/arches/mobile_project_manager/mobile_project_manager.js
+++ js/arches/mobile_project_manager/mobile_project_manager.js
@@ -5,13 +5,55 @@
 
 //	Add Functions
 	$('#btn-save-edits').on('click', function (ev) {
+		ev.preventDefault();
 
 		//Manage Display of Project listing
 		$("#default-message-panel").addClass("hidden");
 		$("#project-listing-panel").removeClass("hidden");
 
-
 	});	
+
+
+	$('.get-project-detail').on('click', function (ev) {
+		ev.preventDefault();
+
+		//Manage Display of Project listing
+		$("#resource-cards").addClass("hidden");
+		$("#card-detail").removeClass("hidden");
+
+	});
+
+	$('#card-detail').on('click', function (ev) {
+		ev.preventDefault();
+
+		//Manage Display of Project listing
+		$("#resource-cards").removeClass("hidden");
+		$("#card-detail").addClass("hidden");
+
+	});
+
+	$('.get-user-detail').on('click', function (ev) {
+		ev.preventDefault();
+
+		//Manage Display of Project listing
+		$("#user-cards").addClass("hidden");
+		$("#user-detail").removeClass("hidden");
+
+	});
+
+	$('#user-detail').on('click', function (ev) {
+		ev.preventDefault();
+
+		//Manage Display of Project listing
+		$("#user-cards").removeClass("hidden");
+		$("#user-detail").addClass("hidden");
+
+	});
+
+
+
+
+	
 
 // http://abpetkov.github.io/switchery/
 	new Switchery(document.getElementById('demo-sw-unchecked'), { size: 'small' });
